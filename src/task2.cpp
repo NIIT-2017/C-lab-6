02c0920fccdf22b3a029246bcd7d6bb4fed37f17
beelt <anikipelov@gmail.com>
2020-01-20 22:04:40 +0300

    Корректировка задачи №2

--- src/task2.cpp
+++ src/task2.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-unsigned int collatz(unsigned long long num, unsigned int i)
+unsigned int collatz(unsigned long long num)
 {
+    static int count = 0;
+    count++;
     if (num == 1)
-        return i;
+        return num;
     else if (num % 2 != 0)
-        return collatz(3 * num + 1, i + 1);
+        return collatz(3 * num + 1) + 1;
     else if (num % 2 == 0)
-        return collatz(num / 2, i + 1);
+        return collatz(num / 2) + 1;
+    return count;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen)
@@ -16,7 +19,7 @@ unsigned int seqCollatz(unsigned int *maxlen)
     unsigned long long number = 2;
     for (unsigned long long i = 2; i < 1000000; i++)
     {
-        len = collatz(i, 1);
+        len = collatz(i);
         if (len > *maxlen)
         {
             *maxlen = len;
