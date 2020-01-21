b2404a38a0bc6f5d66466d8a97989bb0634e07ee
dkrasnykh <krasnykh.daria@gmail.com>
2020-01-21 16:12:01 +0300

    b30

--- src/task6.cpp
+++ src/task6.cpp
@@ -1,16 +1,21 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include "task6.h"
 
-unsigned long long innerFib(unsigned long long f1, unsigned long long f2, int N, int i)
+unsigned long long innerFib(unsigned long long f1, unsigned long long f2, int N)
 {
-	if (i == N-1)
+
+	if (N==1)
 		return f2;
 	else
-		return innerFib(f2, f1 + f2, N, i + 1);
+	{
+		//N--;
+		return innerFib(f2, f1 + f2, --N);
+	}
+		
 }
 
 unsigned long long fib2(int N)
 {
-	return innerFib(0, 1, N, 0);
+	return innerFib(0, 1, N);
 }
 
