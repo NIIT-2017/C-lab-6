5a940ac70aa093c4791841db3c447646291a2ad1
dkrasnykh <krasnykh.daria@gmail.com>
2020-01-22 12:10:25 +0300

    b30

--- src/main5.cpp
+++ src/main5.cpp
@@ -10,8 +10,8 @@ int main()
 	rand();
 	FILE* out;
 	out = fopen("task5.txt", "w");
-	printf("%2s %6s \ t%10s\n","=l","=t1","=N1");
-	fprintf(out, "%2s %6s \ t%10s\n", "=l", "=t1", "=N1");
+	printf("%2s %6s  t%10s\n","=l","=t1","=N1");
+	fprintf(out, "%2s %6s  t%10s\n", "=l", "=t1", "=N1");
 	unsigned long long sumfib=0;
 
 	for (int len = 1; len <= SIZE; len ++)
@@ -19,8 +19,8 @@ int main()
 		clock_t begin1 = clock();
 		sumfib = fib1(len);
 		clock_t end1 = clock();
-		printf("%2d  %6.2f \ t %10llu\n", len, (float)(end1 - begin1) / CLOCKS_PER_SEC, sumfib);
-		fprintf(out, "%2d  %6.2f \ t %10llu\n", len, (float)(end1 - begin1) / CLOCKS_PER_SEC, sumfib);
+		printf("%2d  %6.2f  t %10llu\n", len, (float)(end1 - begin1) / CLOCKS_PER_SEC, sumfib);
+		fprintf(out, "%2d  %6.2f  t %10llu\n", len, (float)(end1 - begin1) / CLOCKS_PER_SEC, sumfib);
 	}
 	fclose(out);
 	return 0;
