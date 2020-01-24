228b8c28d4ce2c2c6cd1ffa9e733ff38665b1233
beelt <anikipelov@gmail.com>
2020-01-24 12:21:01 +0300

    Выполнена задача №8

---
+++ src/task8.cpp
@@ -0,0 +1,63 @@
+#include "task8.h"
+#include <stdlib.h>
+#include <string.h>
+
+int eval(char* buf)
+{
+    if (buf[0] != '(')
+        return atoi(buf);
+    char a1[256], a2[256];
+    char op;
+    op = partition(buf, a1, a2);
+    switch (op)
+    {
+    case '+':
+        return eval(a1) + eval(a2);
+    case '-':
+        return eval(a1) - eval(a2);
+    case '*':
+        return eval(a1) * eval(a2);
+    case '/':
+        return eval(a1) / eval(a2);
+    }
+}
+
+char partition(char* buf, char* expr1, char* expr2)
+{
+    int i = 1, count = 0;
+    char op;
+
+    while (buf[i])
+    {
+        if (buf[i] == '(')
+            count++;
+        else if (buf[i] == ')')
+            count--;
+        else if (count == 0 && !(buf[i] >= '0' && buf[i] <= '9'))
+        {
+            op = buf[i];
+
+            // copy first operand
+            int j = 0;
+            while (j < i - 1)
+            {
+                expr1[j] = buf[j + 1];
+                j++;
+            }
+            expr1[j + 1] = '\0';
+
+            // copy second operand
+            int k = i + 1, m = 0;
+            while (k < strlen(buf))
+            {
+                expr2[m] = buf[k];
+                k++;
+                m++;
+            }
+            expr2[m + 1] = '\0';
+            break;
+        }
+        i++;
+    }
+    return op;
+}
