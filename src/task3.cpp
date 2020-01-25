b069921f2d1368eca020392fa438552f653344f6
dkrasnykh <krasnykh.daria@gmail.com>
2020-01-25 21:23:38 +0300

    b30

--- src/task3.cpp
+++ src/task3.cpp
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "task3.h"
 
+static int count;
+/*
 char* addChar(char* buf, char c)
 {
 	int len = 0;
@@ -11,9 +13,25 @@ char* addChar(char* buf, char c)
 	buf[len] = '\0';
 	return buf;
 }
+*/
+char* addChar(char* buf, char c)
+{
+	int len = 0;
+	//len = strlen(buf) //strlen не использовалась, т.к. в условии задачи есть запрет использования библиотечных функций
+	while (buf[len])
+		len++;
+	buf[len++] = c;
+	buf[len] = '\0';
+	return buf;
+}
+
 
 char* int2str(char* buf, unsigned int value)
 {
+	if (count == 0)
+		buf[0] = '\0';
+	count++;
+
 	if (value / 10 == 0)
 		return addChar(buf, '0' + value % 10);
 	int2str(buf, value / 10);
