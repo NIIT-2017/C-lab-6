26e4ad734797b26d8000a66a63582587b71ebe9e
Evgeny <57017613+ekaryagin@users.noreply.github.com>
2020-01-26 16:01:38 +0300

    Update task8.cpp

--- src/task8.cpp
+++ src/task8.cpp
@@ -8,17 +8,13 @@ char partition(char* buf, char* expr1, char* expr2)
 		i1 = 0,
 		i2 = 0,
 		j = 0;
-	
-	// удаляем лишние скобки в начале и в конце
-	for (int i = 0; i < strlen(buf); i++)
+	for (int i = 0; i < (strlen(buf)-1); i++) // удаляем лишние скобки в начале и в конце (раскрываем переданное выражение)
 		buf[i] = buf[i + 1];
 	buf[strlen(buf) - 1] = '\0';
-
-	// записываем первый аргумент
-	if (buf[0] != '(')
+	if (buf[0] != '(') 	// записываем первый аргумент - добрались до цифры
 		for (; buf[j] >= 48 && buf[j] <= 57; j++, i1++)
 			expr1[i1] = buf[j];
-	else
+	else  // записываем первый аргумент - если ещё не добрались до цифры
 	{
 		for (int k = 0; !(buf[k] >= 48 && buf[k] <= 57); k++)
 			parenthesis++;
@@ -29,14 +25,11 @@ char partition(char* buf, char* expr1, char* expr2)
 				parenthesis--;
 		}
 	}
-	
-	// записываем арифметический знак
-	sign = buf[j++]; 
-
-	// записываем второй аргумент
-	for (; buf[j]; j++, i2++)
+	expr1[i1] = '\0';
+	sign = buf[j++];  // записываем арифметический знак
+	for (; buf[j]; j++, i2++) // записываем второй аргумент
 		expr2[i2] = buf[j];
-		
+	expr2[i2] = '\0';
 	return sign;
 }
 
@@ -44,12 +37,9 @@ int eval(char* buf)
 {
 	if (buf[0] != '(') // если добрались до цифры
 		return atoi(buf);
-	
 	char a1[256]{}, a2[256]{};
 	char op = '+';
-	
 	op = partition(buf, a1, a2);
-		
 	switch (op)
 	{
 	case '+':
