83b428ea4f7ef8719f844f5dd353d4bac0aefa2f
Koeco97 <komissarovaolesya@yandex.ru>
2020-02-02 21:25:11 +0300

    task3.cpp is changed

--- src/task3.cpp
+++ src/task3.cpp
@@ -4,18 +4,18 @@
 
 char* int2str(char* buf, unsigned int value)
 {
-    if (value > 10)
+    if (value >= 10)
     {
         int2str(buf, value / 10);
         int number = value % 10;
-        buf[strlen(buf)] = '0' + number;
-        buf[strlen(buf) + 1] = '\0';
+        *(buf + strlen(buf) + 1) = '\0';
+        *(buf + strlen(buf)) = '0' + number;
     }
     else
     {
         int number = value % 10;
-        buf[strlen(buf)] = '0' + number;
-        buf[strlen(buf) + 1] = '\0';
+        *buf = '0' + number;
+        *(buf + 1) = '\0';
     }
     return buf;
 }
