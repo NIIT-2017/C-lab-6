8115b84ace9212f3e5fd10bfa1aea9e203d02763
beelt <anikipelov@gmail.com>
2020-02-03 15:59:06 +0300

    Корректировка задачи №2 (2)

--- src/main2.cpp
+++ src/main2.cpp
@@ -3,8 +3,9 @@
 
 int main()
 {
-    unsigned int maxlen;
-    unsigned int num = seqCollatz(&maxlen);
+    unsigned int maxlen = 0;
+    unsigned int * p = &maxlen;
+    unsigned int num = seqCollatz(p);
 
     printf("Number is %u, max length is %u\n", num, maxlen);
 
