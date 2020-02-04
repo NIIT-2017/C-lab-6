76abf70f1ed098a65efaa0fd3268bac6e3c3592a
EveerDrof <EveerDrof@git.com>
2020-02-04 20:37:51 +0300

    Repaired task6

--- src/task6.cpp
+++ src/task6.cpp
@@ -9,7 +9,9 @@ void fibFromStart(int i, long long* arr, int n)
 unsigned long long fib2(int N)
 {
     long long * arr = (long long*)malloc(N * sizeof(long long));
-    arr[0] =1 ;
+    if (N == 0||N==1)
+        return 1;
+    arr[0] = 1;
     arr[1] = 1;
     fibFromStart(2, arr, N);
     return  arr[N-1];
