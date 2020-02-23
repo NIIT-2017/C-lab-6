2f7720d5cbf73c598ad079a70d2738940872b035
Oigul <aj3913@yandex.ru>
2020-02-23 04:48:13 +0300

    C-lab-6

--- src/task6.cpp
+++ src/task6.cpp
@@ -1,13 +1,19 @@
-unsigned long long fib1(unsigned long long f1, unsigned long long f2, int N)
+#include <stdio.h>
+
+unsigned long long fib1(int f1, int f2, int N)
 {
-	if (N == 1)
-		return 1;
-	else if (N == 2)
+	if (N == 1) 
+	{
 		return f2;
-	else
+	}
+	else 
+	{
 		return fib1(f2, f2 + f1, N - 1);
+	}
 }
-unsigned long long fib2(int N)
+
+
+unsigned long long fib2(int N) 
 {
-	return (fib1(1, 1, N));
+	return fib1(0, 1, N);
 }
