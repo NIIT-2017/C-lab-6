763b5d19a56c6a4463666a56000a93ca3635f28c
Oigul <aj3913@yandex.ru>
2020-02-23 14:29:30 +0300

    c-lab-6

---
+++ src/main1.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "task1.h"
+#define SIZE 3
+
+int main() 
+{
+	char arr[M][N];
+	for (int i = 0; i < N; i++) 
+	{
+		for (int j = 0; j < M; j++) 
+		{
+			arr[i][j] = ' ';
+		}
+	}
+
+	drawFractal(arr, M / 2, N / 2, SIZE);
+
+	for (int i = 0; i < M; i++) 
+	{
+		for (int j = 0; j < M; j++)
+		{
+			printf("%2c", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
---
+++ src/task8.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 256
+
+char partition(char* buf, char* expr1, char* expr2)
+{
+	int opening = 0, closing = 0;
+	buf[strlen(buf) - 1] = '\0';
+	int length = strlen(buf);
+	int sign = 0, isign = 0;
+	
+	for (int i = 0; i <= length; i++)
+	{
+		if (buf[i] == '(')
+			opening++;
+		else if (buf[i] == ')')
+			closing++;
+		else
+		if ((buf[i] == '-' || buf[i] == '+' || buf[i] == '*' || buf[i] == '/') && ((opening - 1) == closing))
+		{
+			sign = buf[i];
+			isign = i;
+		}
+	}
+
+	int j = 0;
+	for (int i = 1; i < isign; i++)
+	{
+		expr1[j] = buf[i];
+		j++;
+	}
+	j = 0;
+	for (int i = isign + 1; i < length; i++)
+	{
+		expr2[j] = buf[i];
+		j++;
+	}
+	
+	return sign;
+}
+
+int eval(char* buf)
+{
+	if (buf[0] != '(') 
+		return atoi(buf); // number
+	char expr1[SIZE] = { 0 }, expr2[SIZE] = { 0 };
+	char sign;
+	sign = partition(buf, expr1, expr2);
+	switch (sign)
+	{
+	case '+':
+		return eval(expr1) + eval(expr2);
+	case '-':
+		return eval(expr1) - eval(expr2);
+	case '*':
+		return eval(expr1) * eval(expr2);
+	case '/':
+		return eval(expr1) / eval(expr2);
+	}
+}
