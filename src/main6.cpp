55b7a094c50312d4c31db1c246b1e085f69e3569
Ekaterina-Treushnikova <57718887+Ekaterina-Treushnikova@users.noreply.github.com>
2020-02-27 21:23:03 +0300

    Update main6.cpp

--- src/main6.cpp
+++ src/main6.cpp
@@ -6,10 +6,10 @@ int main()
 {
 	for (int N = 1; N < 40; N++)
 	{
-	clock_t begin = clock();
+	//clock_t begin = clock();
 	fib2(N);
-	clock_t end = clock();
-	printf("%4d\t%-10llu  \t%3.4f\n", N, fib2(N), (float)(end - begin) / CLOCKS_PER_SEC);
+	//clock_t end = clock();
+	printf("%4d\t%-10llu\n", N, fib2(N)); //(float)(end - begin) / CLOCKS_PER_SEC);
 	}
 
 	return 0;
