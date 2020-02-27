8e604d89f333e75aef073515db7a1c9a797b01a9
Ekaterina-Treushnikova <57718887+Ekaterina-Treushnikova@users.noreply.github.com>
2020-02-27 21:36:06 +0300

    Update main6.cpp

--- src/main6.cpp
+++ src/main6.cpp
@@ -7,7 +7,7 @@ int main()
 	for (int N = 1; N < 40; N++)
 	{
 	//clock_t begin = clock();
-	fib2(N);
+	//fib2(N);
 	//clock_t end = clock();
 	printf("%4d\t%-10llu\n", N, fib2(N)); //(float)(end - begin) / CLOCKS_PER_SEC);
 	}
