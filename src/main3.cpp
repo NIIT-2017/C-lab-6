d479f7d33969ebf4e6736f1f508d0039dca16ad0
Bear346 <57682132+Bear346@users.noreply.github.com>
2020-03-05 00:14:03 +0300

    Update main3.cpp

--- src/main3.cpp
+++ src/main3.cpp
@@ -8,7 +8,7 @@ int  main()
 	unsigned int value;
 	char buf[N] = { 0 };
 	puts("Enter the number!");
-	scanf("%u", &value);
+	scanf("%d", &value);
 	printf("Digits were transformed to string : %s\n", int2str(buf, value));
 	return 0;
 }
